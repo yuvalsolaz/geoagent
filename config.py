ce8c532ab7a5030d2c0cc5724895a178c0088000
yuval solaz <yuval.solaz@gmail.com>
2025-01-26 14:13:55 +0200

    google gemini llm support

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 # Access OpenAI API key
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 LLM_MODEL = os.getenv("LLM_MODEL", "gpt-4o-mini")
 LLM_TEMPERATURE = 0
 
--- models/llm.py
+++ models/llm.py
@@ -1,5 +1,15 @@
 from langchain_openai import ChatOpenAI
-from config import OPENAI_API_KEY, LLM_MODEL, LLM_TEMPERATURE
+from langchain_google_genai import ChatGoogleGenerativeAI
 
+from config import OPENAI_API_KEY, GOOGLE_API_KEY, LLM_MODEL, LLM_TEMPERATURE
 
-llm = ChatOpenAI(model=LLM_MODEL, temperature=LLM_TEMPERATURE, openai_api_key=OPENAI_API_KEY)
+if GOOGLE_API_KEY:
+    llm = ChatGoogleGenerativeAI(
+                 model="gemini-1.5-pro",
+                 temperature=LLM_TEMPERATURE,)
+elif OPENAI_API_KEY:
+    llm = ChatOpenAI(model=LLM_MODEL,
+                 temperature=LLM_TEMPERATURE,
+                 openai_api_key=OPENAI_API_KEY)
+else:
+    raise Exception("Error: API KEY missing. define API_KEY environment variable")
