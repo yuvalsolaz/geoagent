05b1d487a93c46b5cf8e29968a8c1313a69f795e
yuval solaz <yuval.solaz@gmail.com>
2025-02-05 11:10:09 +0200

    fix map html

--- evaluate_benchmark.py
+++ evaluate_benchmark.py
@@ -3,18 +3,13 @@ import pandas as pd
 from models.geo_agent import GeoAgent
 from tools.visualize_on_map_tool import create_map
 
-def evaluate_queries(df):
 
+def evaluate_queries(df):
     agent = GeoAgent()
 
     def visualize():
         m = create_map()
-        map_html = m._repr_html_()
-        map_html = map_html.replace("position:relative;width:100%;height:0;padding-bottom:60%;",
-                                    "position:relative;width:100%;height:0;padding-bottom:100%;", 1)
-        map_html = map_html.replace("height: 500.0px;",
-                                    "height: 800.0px;",1)
-        return map_html
+        return m._repr_html_()
 
     def evaluate_query(row):
         # if 'SELECT' in row.get('sql',''):
@@ -26,6 +21,7 @@ def evaluate_queries(df):
     df[['sql', 'map']] = df.apply(evaluate_query, axis=1)
     return df
 
+
 def load_dataset(benchmark_file_name):
     try:
         df = pd.read_csv(benchmark_file_name)
@@ -33,6 +29,7 @@ def load_dataset(benchmark_file_name):
     except Exception as ex:
         print(f'error loading {benchmark_file_name}: {ex} ')
         return None
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -42,9 +39,8 @@ if __name__ == "__main__":
     print(f'loading {benchmark_file_name}...')
     df = load_dataset(benchmark_file_name=benchmark_file_name)
     print(f'{df.shape[0]} geo queries loaded. evaluating...')
-    df = evaluate_queries(df[:5])
+    df = evaluate_queries(df)
     print('Evaluate queries finished')
-    output_file = benchmark_file_name.replace('.csv', '_sql.csv')
+    output_file = benchmark_file_name.replace('.csv', '_map.csv')
     print(f'Evaluate queries finished, save results to {output_file}')
     df.to_csv(output_file)
-
